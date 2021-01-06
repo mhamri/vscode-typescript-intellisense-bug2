db2ffce8beee76045f90e2608ff62c77cfe7968e
mhamriweb@gmail.com <mhamriweb@gmail.com>
2021-01-06 22:11:35 +0800

    install angular state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Sample1Component } from './sample1/sample1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Sample1Component
   ],
   imports: [
     BrowserModule,
