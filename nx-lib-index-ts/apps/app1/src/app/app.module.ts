8fcea4f7796a79626a1249d59978a8962fc7fbc9
mhamriweb@gmail.com <mhamriweb@gmail.com>
2021-01-27 16:56:39 +0800

    add readme.md

--- nx-lib-index-ts/apps/app1/src/app/app.module.ts
+++ nx-lib-index-ts/apps/app1/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// import { SampleModule } from "@myApp/lib1-name";
+import { SampleModule } from "@myApp/lib1-name";
 import { AppComponent } from './app.component';
 
 
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # A repo of different vscode typescript intellisence bugs 
 
+to use this repo, please change to any of these sub folders and open the vscode from those folders. 
 
 ### [Angular second level imports ](./second-level-node-module-libs)
 
